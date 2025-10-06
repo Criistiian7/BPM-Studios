28c3c169829ba4032a8c8b14b890db88375f7e2d
Criistiian7 <ana.cristian94@yahoo.com>
2025-10-06 17:52:02 +0200

    reconfigurare tailwind

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -28,7 +28,7 @@ function Login() {
         {error && (
           <div
             className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-3"
-            role="aler"
+            role="alert"
           >
             <span className="block sm:inline">{error}</span>
           </div>
