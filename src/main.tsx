31eefd3633a1eb07b8e88fdff3d0e132ae2b92fa
AdyHap999 git config --global user.email <adyhap94@gmail.com>
2025-10-08 16:29:41 +0300

    update route la fisierul main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// linia 3 cu 4 sunt importurile de dinainte momentan.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/authcontext";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- src/pages/authpage.tsx
+++ src/pages/authpage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Login from "../components/auth/login";
 import Register from "../components/auth/register";
 
-const AuthPage: React.FC = () => {
+const authpage: React.FC = () => {
     const  [mode, setMode] = useState<"login" | "register">("login");
 
     return (
