dc8123a27b467608fa36dbb68e1a29fb228fc545
AdyHap999 git config --global user.email <adyhap94@gmail.com>
2025-10-08 15:38:12 +0300

    ajustări UI la authpage.tsx

--- src/pages/authpage.tsx
+++ src/pages/authpage.tsx
@@ -6,14 +6,25 @@ const AuthPage: React.FC = () => {
     const  [mode, setMode] = useState<"login" | "register">("login");
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+        <div className="min-h-screen flex items-center justify-center 
+        bg-gradient-to-br from-gray-50 to-gray-100 p-6">
+        <div className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8 items-center"> 
+            <div className="hidden md:flex flex-col items-start p-8">
+                <h1 className="text-4xl font-extrabold mb-4">Beat Planner</h1>
+                <p className="text-gray-600">Conectează-te cu artiști, împărtășește 
+                    track-uri și construiește rețeaua ta.</p>
+            </div>
+            
+            <div class="flex items-center justify-center">
             {mode === "login" ? (
                 <Login onSwitchToRegister={() => setMode("register")} />
             ) : (
                 <Register onSwitchToLogin={() => setMode("login")} />
             )}
             </div>
-            );
-        };
+        </div>
+    </div>
+    );
+};
         
-        export default AuthPage;
+export default authpage;
